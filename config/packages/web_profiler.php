1b123051b398e8a9244d487d6c910d490e16a30a
Tac Tacelosky <tacman@gmail.com>
2022-09-22 14:15:17 -0400

    update libraries

--- config/packages/web_profiler.php
+++ config/packages/web_profiler.php
@@ -7,7 +7,9 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 return static function (ContainerConfigurator $containerConfigurator): void {
     if ($containerConfigurator->env() === 'dev') {
         $containerConfigurator->extension('web_profiler', ['toolbar' => true, 'intercept_redirects' => false]);
-        $containerConfigurator->extension('framework', ['profiler' => ['only_exceptions' => false, 'collect_serializer_data' => true]]);
+        $containerConfigurator->extension('framework', ['profiler' => ['only_exceptions' => false,
+            'collect' => true
+        ]]);
     }
     if ($containerConfigurator->env() === 'test') {
         $containerConfigurator->extension('web_profiler', ['toolbar' => false, 'intercept_redirects' => false]);

a71b285110d54b541bf4f5d1425745b63895bcda
Hryhoruk <antonhryhoruk13@gmail.com>
2023-05-08 17:58:10 +0300

    removed headers

--- packages/grid-group-bundle/tests/ParserTest.php
+++ packages/grid-group-bundle/tests/ParserTest.php
@@ -2,6 +2,7 @@
 
 namespace Survos\GridGroupBundle\Tests;
 
+use League\Csv\Reader;
 use PHPUnit\Framework\TestCase;
 use Survos\GridGroupBundle\CsvSchema\Parser;
 use Symfony\Component\Yaml\Yaml;
@@ -10,33 +11,20 @@ use function PHPUnit\Framework\assertEquals;
 
 class ParserTest extends TestCase
 {
-    /**
-     * @dataProvider csvSteps
-     */
-    public function testParser(array $test)
+    public function testParser()
     {
-        $header = $test['headers'];
-        $schema = Parser::createSchemaFromMap($test['map'] ?? [], $header);
-
-        $parser = new Parser([
-            'schema' => $schema
-        ]);
-
-        $data = $test['source'] ?? null;
-        $expects = $test['expects'] ?? null;
-
-        foreach ($parser->fromString($data) as $actual) {
-            $expects = json_decode($test['expects'], true);
-            $this->assertSame($expects, $actual);
-            assert($expects, "invalid json: " . $test['expects']);
-        }
-    }
-
-    public static function csvSteps()
-    {
-        $data = Yaml::parseFile(__DIR__ . '/parser-test.yaml');
-        foreach ($data['tests'] as $key => $test) {
-            yield [$key => $test];
+        $yaml = Yaml::parseFile(__DIR__ . '/parser-test.yaml');
+        foreach ($yaml['tests'] as $test) {
+            $csvString = $test['source'];
+            $csvReader = Reader::createFromString($csvString)->setHeaderOffset(0);
+            $schema = Parser::createSchemaFromMap($test['map'] ?? [], $csvReader->getHeader());
+            $config['schema'] = $schema;
+            $parser = new Parser($config);
+            foreach ($parser->fromString($csvString) as $row) {
+                $expects = json_decode($test['expects'], true);
+                assert($expects, "invalid json: " . $test['expects']);
+                assertEquals($expects, $row, json_encode($expects) . '<>' . json_encode($row));
+            }
         }
     }
 }

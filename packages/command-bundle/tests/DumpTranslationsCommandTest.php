3cb4d60c458eefd6a8e996a9576ecad9b4066e62
Hryhoruk <antonhryhoruk13@gmail.com>
2023-05-11 14:32:08 +0300

    WIp

--- packages/command-bundle/tests/DumpTranslationsCommandTest.php
+++ packages/command-bundle/tests/DumpTranslationsCommandTest.php
@@ -2,7 +2,6 @@
 
 namespace Survos\CommandBundle\Tests;
 
-use Survos\CommandBundle\Command\DumpTranslationsCommand;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
@@ -12,14 +11,12 @@ class DumpTranslationsCommandTest extends KernelTestCase
     public function testExecute()
     {
         $kernel = self::bootKernel();
-
         $application = new Application($kernel);
-        $application->add(new DumpTranslationsCommand($kernel, ['survos']));
 
         $command = $application->find('survos:command:dump-as-messages');
         $commandTester = new CommandTester($command);
         $commandTester->execute([
-            '--namespace' => 'app'
+            '--namespace' => 'survos'
         ]);
 
         $commandTester->assertCommandIsSuccessful();

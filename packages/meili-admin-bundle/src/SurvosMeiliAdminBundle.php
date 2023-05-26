daa69b3c3c02bfed602fff750b9b265fc59d18a3
Tac Tacelosky <tacman@gmail.com>
2023-05-26 05:24:44 -0400

    add meili admin

--- packages/meili-admin-bundle/src/SurvosMeiliAdminBundle.php
+++ packages/meili-admin-bundle/src/SurvosMeiliAdminBundle.php
@@ -2,6 +2,7 @@
 
 namespace Survos\MeiliAdminBundle;
 
+use Survos\MeiliAdminBundle\Controller\MeiliAdminController;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -14,6 +15,11 @@ class SurvosMeiliAdminBundle extends AbstractBundle
 
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
+
+        $builder->autowire(MeiliAdminController::class)
+            ->setAutoconfigured(true)
+            ->setPublic(true)
+        ;
 
         // $builder->setParameter('survos_workflow.direction', $config['direction']);
 

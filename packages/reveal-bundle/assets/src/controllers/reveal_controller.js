20537e2f27f54777a9055d70a8937d1fdbde4ab8
Tac Tacelosky <tacman@gmail.com>
2023-07-24 10:24:31 -0400

    add reveal-bundle

---
+++ packages/reveal-bundle/assets/src/controllers/reveal_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from '@hotwired/stimulus';
+import Reveal from 'reveal.js'
+
+/*
+* The following line makes this controller "lazy": it won't be downloaded until needed
+* See https://github.com/symfony/stimulus-bridge#lazy-controllers
+*/
+/* stimulusFetch: 'lazy' */
+export default class extends Controller {
+    static targets = ['slideshow']
+    static values = {
+        slides: Array,
+    }
+
+    connect() {
+        super.connect();
+        let el = this.element;
+        // if (this.hasSlideshowTarget) {
+        //     // this.slideshowTarget.innerHTML = "…"
+        //     // this.slideshowTarget.innerHTML = 'test';
+        // } else {
+        //     console.error('missing slideshowTarget');
+        // }
+        console.warn("hello from " + this.identifier);
+
+        let orig = new Reveal(el, {
+            //     hash: true,
+            //     // Learn about plugins: https://revealjs.com/plugins/
+            //     plugins: [ ]
+
+        });
+        orig.initialize();
+
+    }
+        // ...
+}

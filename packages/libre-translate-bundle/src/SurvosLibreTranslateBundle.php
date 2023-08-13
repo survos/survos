61c77199f126302259642dbc40a820ae19746211
Tac Tacelosky <tacman@gmail.com>
2023-08-13 15:29:50 -0600

    prepare release

--- packages/libre-translate-bundle/src/SurvosLibreTranslateBundle.php
+++ packages/libre-translate-bundle/src/SurvosLibreTranslateBundle.php
@@ -22,6 +22,12 @@ class SurvosLibreTranslateBundle extends AbstractBundle
             ->setPublic(true);
         $definition->setArgument('$host', $config['host']);
         $definition->setArgument('$port', $config['port']);
+        if ($apiKey = $config['apikey']) {
+            $definition->addMethodCall('setApiKey', [$apiKey]);
+        }
+        if ($source = $config['apikey']) {
+            $definition->addMethodCall('setSource', [$apiKey]);
+        }
 
         // twig classes
 
@@ -44,10 +50,9 @@ class SurvosLibreTranslateBundle extends AbstractBundle
             ->scalarNode('host')->defaultValue('http://127.0.0.1')->end()
             ->scalarNode('port')->defaultValue('5000')->end()
             ->scalarNode('apikey')->defaultValue(null)->end()
-            ->arrayNode('entities')
-            ->scalarPrototype()
-            ->end()->end()
-            ->booleanNode('enabled')->defaultTrue()->end()
+            ->scalarNode('sourceLanguage')->defaultValue(null)->end()
+            ->scalarNode('targetLanguage')->defaultValue(null)->end()
+            ->booleanNode('canManage')->defaultTrue()->end()
 //            ->integerNode('min_sunshine')->defaultValue(3)->end()
             ->end();
     }

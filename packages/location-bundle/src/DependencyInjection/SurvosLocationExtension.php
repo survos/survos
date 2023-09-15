73b6b0150d55b52ff765cea26eec3b0de6dd9e07
Tac Tacelosky <tacman@gmail.com>
2023-09-15 08:23:28 -0400

    add void to avoid deprecations

--- packages/location-bundle/src/DependencyInjection/SurvosLocationExtension.php
+++ packages/location-bundle/src/DependencyInjection/SurvosLocationExtension.php
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class SurvosLocationExtension extends Extension implements PrependExtensionInterface
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
 
 //        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));

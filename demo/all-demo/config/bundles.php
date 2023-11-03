e4f7fae9addbb537c52f3f7b063ed7df080099a9
Tac Tacelosky <tacman@gmail.com>
2023-11-03 05:19:01 -0400

    initial version for all-demo

---
+++ demo/all-demo/config/bundles.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Survos\CoreBundle\SurvosCoreBundle::class => ['all' => true],
+    Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
+    Survos\BootstrapBundle\SurvosBootstrapBundle::class => ['all' => true],
+    Survos\CrawlerBundle\SurvosCrawlerBundle::class => ['all' => true],
+    Survos\Bundle\MakerBundle\SurvosMakerBundle::class => ['dev' => true, 'test' => true],
+    Survos\Scraper\SurvosScraperBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Survos\Grid\SurvosGridBundle::class => ['all' => true],
+    Survos\SimpleDatatables\SurvosSimpleDatatablesBundle::class => ['all' => true],
+];
---
+++ demo/all-demo/src/EventListener/AppMenuMenuEventListener.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\EventListener;
+
+use Survos\BootstrapBundle\Event\KnpMenuEvent;
+use Survos\BootstrapBundle\Traits\KnpMenuHelperInterface;
+use Survos\BootstrapBundle\Traits\KnpMenuHelperTrait;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+#[AsEventListener(event: KnpMenuEvent::PAGE_MENU_EVENT, method: 'pageMenu')]
+#[AsEventListener(event: KnpMenuEvent::NAVBAR_MENU_EVENT, method: 'navbarMenu')]
+final class AppMenuMenuEventListener implements KnpMenuHelperInterface
+{
+    use KnpMenuHelperTrait;
+
+    // this should be optional, not sure we really need it here.
+    public function __construct(private ?AuthorizationCheckerInterface $security = null)
+    {
+    }
+
+    public function navbarMenu(KnpMenuEvent $event): void
+    {
+        $menu = $event->getMenu();
+        $options = $event->getOptions();
+
+//        $this->add($menu, 'app_homepage');
+        // for nested menus, don't add a route, just a label, then use it for the argument to addMenuItem
+
+        $nestedMenu = $this->addSubmenu($menu, 'Credits');
+
+        foreach (['bundles', 'javascript'] as $type) {
+            // $this->addMenuItem($nestedMenu, ['route' => 'survos_base_credits', 'rp' => ['type' => $type], 'label' => ucfirst($type)]);
+            $this->addMenuItem($nestedMenu, ['uri' => "#$type", 'label' => ucfirst($type)]);
+        }
+    }
+
+
+public function pageMenu(KnpMenuEvent $event): void
+{
+}
+}

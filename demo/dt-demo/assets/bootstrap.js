25ab1c8f21fe47b367411a981894e07ce8a00657
Tac Tacelosky <tacman@gmail.com>
2023-11-15 11:56:25 -0500

    remove dt-demo, now in survos-sites

--- demo/dt-demo/assets/bootstrap.js
+++
@@ -1,6 +0,0 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
-
-const app = startStimulusApp();
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
-app.debug = true; // process.env.NODE_ENV === 'development'

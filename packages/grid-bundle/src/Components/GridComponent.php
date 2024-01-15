ac7e8aaabe0b6fb7ad20514cf2e5c7f92c305607
Tac Tacelosky <tacman@gmail.com>
2024-01-15 18:26:45 -0500

    add dom

--- packages/grid-bundle/src/Components/GridComponent.php
+++ packages/grid-bundle/src/Components/GridComponent.php
@@ -54,6 +54,7 @@ class GridComponent
             'tableId' => null,
             'tableClasses' => '',
             'scrollY' => '50vh',
+            'dom' => null,
 //            'stimulusController' => '@survos/grid-bundle/grid',
             'search' => true,
             'info' => false,

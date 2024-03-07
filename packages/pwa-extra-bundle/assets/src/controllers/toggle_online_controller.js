ce4309aa6b3c36f2787fa5bda6a0aa9eca76ca8e
Tac Tacelosky <tacman@gmail.com>
2024-03-07 10:54:14 -0600

    fix online toggle

--- packages/pwa-extra-bundle/assets/src/controllers/toggle_online_controller.js
+++ packages/pwa-extra-bundle/assets/src/controllers/toggle_online_controller.js
@@ -1,15 +1,16 @@
 import {Controller} from '@hotwired/stimulus';
 
-// /* stimulusFetch: 'lazy' */
+/* stimulusFetch: 'lazy' */
 export default class extends Controller {
     static targets = ['online', 'offline']
 
     connect() {
-        console.log('navigator status at connect', navigator.onLine);
+        console.warn('navigator status at connect', navigator.onLine);
         if (navigator.onLine) {
             this.showOnline();
         } else {
-            this.showOffline();
+            // this.showOffline();
+            this.showOnline();
         }
 
         window.ononline = (event) => {
@@ -20,6 +21,7 @@ export default class extends Controller {
         };
 
         window.addEventListener("offline", () => {
+            this.showOnline();
             this.showOffline();
         });
         window.addEventListener("online", () => {
@@ -33,26 +35,32 @@ export default class extends Controller {
     }
 
     onlineTargetConnected(element) {
-        console.log(element);
+        console.log('connecting an onlineTarget! set ' + element.id + ' style.display to ');
+        element.style.display = navigator.onLine ? "block" : "none";
+
         // let countryCode = element.innerText;
     }
 
 
     showOnline() {
-        console.log('online');
+        const display = navigator.onLine ? "block" : "none";
+        console.log('Fix targets ' + display);
         // go through all the targets and hide/show as appropriate
-        if (this.hasOnlineTargets) {
-            this.onlineTargets.foreach((t) => {
-                    t.hidden = false;
+        console.error(this.hasOnlineTarget);
+        if (this.hasOnlineTarget) {
+            console.log(this.onlineTargets.length + ' online targets');
+            this.onlineTargets.forEach((t) => {
+                    console.log(t.id, display);
+                    t.style.display = display;
                 }
             );
         } else {
-            // console.error('no language targets in ' + this.identifier);
+            console.error('no online targets in ' + this.identifier);
         }
     }
 
     showOffline() {
-        console.log('offline');
+        console.log('fixing offline only elements');
         // this.offlineTarget.hidden = false
         // this.onlineTarget.hidden = true
         // this.cleanupClasses();

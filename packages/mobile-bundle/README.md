c79efd117b6b594cc2c10cf8031947d5c128ebae
Tac Tacelosky <tacman@gmail.com>
2024-04-17 10:03:12 -0400

    add mobile-bundle

---
+++ packages/mobile-bundle/README.md
@@ -0,0 +1,9 @@
+# Survos Mobile Bundle
+
+A collection of tools to help create Symfony-based mobile apps.
+
+* OnsenUI 
+* pwa-bundle
+* Dexie
+
+Work in Progress.  See survos-sites/pokemon to see this in action.
---
+++ packages/mobile-bundle/src/SurvosMobileBundle.php
@@ -0,0 +1,39 @@
+<?php
+
+/** generated from /home/tac/g/survos/survos/vendor/survos/maker-bundle/templates/skeleton/bundle/src/Bundle.tpl.php */
+
+namespace Survos\MobileBundle;
+
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class SurvosMobileBundle extends AbstractBundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        // $builder->setParameter('survos_workflow.direction', $config['direction']);
+
+        // twig classes
+
+        /*
+        $definition = $builder
+        ->autowire('survos.barcode_twig', BarcodeTwigExtension::class)
+        ->addTag('twig.extension');
+
+        $definition->setArgument('$widthFactor', $config['widthFactor']);
+        $definition->setArgument('$height', $config['height']);
+        $definition->setArgument('$foregroundColor', $config['foregroundColor']);
+        */
+    }
+
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->rootNode()
+            ->children()
+            ->scalarNode('theme')->defaultValue('pagestack')->end()
+            ->booleanNode('enabled')->defaultTrue()->end()
+            ->end();
+    }
+}

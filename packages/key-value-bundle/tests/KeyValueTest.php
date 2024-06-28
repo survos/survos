943fcdcdf568c7f8b6469d5d2f92a5558e3fd91a
Tac Tacelosky <tacman@gmail.com>
2024-06-28 15:59:03 -0400

    update test

--- packages/key-value-bundle/tests/KeyValueTest.php
+++ packages/key-value-bundle/tests/KeyValueTest.php
@@ -2,6 +2,9 @@
 
 namespace Survos\KeyValueBundle\Tests;
 
+
+// this is in APP, waiting for https://github.com/symfony/symfony/discussions/57587 to move to bundle
+
 use League\Csv\Exception;
 use PHPUnit\Framework\Attributes\Test;
 use Survos\KeyValueBundle\Service\KeyValueService;
@@ -13,23 +16,22 @@ class KeyValueTest extends KernelTestCase
     public function testCreateTables(): void
     {
         $kernel = self::bootKernel([
-//            'environment' => 'test',
+            'environment' => 'test',
         ]);
 
         $this->assertSame('test', $kernel->getEnvironment());
         // $routerService = static::getContainer()->get('router');
         $filename = 'test.pixie.db';
-         $kvService = static::getContainer()->get(KeyValueService::class);
-         $movieTableName = 'movie';
-         $kv = $kvService->getStorageBox($filename, [
-             $movieTableName => 'imdb_id|int,name'
-         ]);
+        $kvService = static::getContainer()->get(KeyValueService::class);
+        $movieTableName = 'movie';
+        $kv = $kvService->getStorageBox($filename, [
+            $movieTableName => 'imdb_id|int,name'
+        ]);
         $this->assertEquals(1, count($kv->getTables()), "bad table count");
-         $kv->select($movieTableName);
-         $this->assertEquals(0, $kv->count(), "$movieTableName should be empty");
-         $this->expectException(\LogicException::class);
-         $kv->select('badTable');
-
+        $kv->select($movieTableName);
+        $this->assertEquals(0, $kv->count(), "$movieTableName should be empty");
+        $this->expectException(\LogicException::class);
+        $kv->select('badTable');
 
 
     }
@@ -45,6 +47,8 @@ class KeyValueTest extends KernelTestCase
         $this->assertNotTrue($indexes['name']->isPrimary, json_encode($indexes['name']));
         $this->assertSame('TEXT', $indexes['name']->type, json_encode($indexes['name']));
         $this->assertSame('TEXT', $indexes['id']->type);
+
+
     }
 
 

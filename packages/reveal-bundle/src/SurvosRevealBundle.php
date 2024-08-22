298ae4292fb3f0fa3a42a80235eb9dafaa609f55
Tac Tacelosky <tacman@gmail.com>
2024-08-22 07:09:25 -0400

    refactor meili admin

--- packages/reveal-bundle/src/SurvosRevealBundle.php
+++ packages/reveal-bundle/src/SurvosRevealBundle.php
@@ -2,6 +2,7 @@
 
 namespace Survos\RevealBundle;
 
+use Survos\CoreBundle\Traits\HasAssetMapperTrait;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -9,34 +10,27 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class SurvosRevealBundle extends AbstractBundle
 {
+    use HasAssetMapperTrait;
+
+    const NAME = 'reveal'; // @todo use this for getPaths()
+
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         // $builder->setParameter('survos_workflow.direction', $config['direction']);
-
-        // twig classes
-
-/*
-$definition = $builder
-->autowire('survos.barcode_twig', BarcodeTwigExtension::class)
-->addTag('twig.extension');
-
-$definition->setArgument('$widthFactor', $config['widthFactor']);
-$definition->setArgument('$height', $config['height']);
-$definition->setArgument('$foregroundColor', $config['foregroundColor']);
-*/
     }
 
     public function configure(DefinitionConfigurator $definition): void
     {
         $definition->rootNode()
             ->children()
-            ->scalarNode('direction')->defaultValue('LR')->end()
-            ->scalarNode('base_layout')->defaultValue('base.html.twig')->end()
-            ->arrayNode('entities')
-            ->scalarPrototype()
-            ->end()->end()
             ->booleanNode('enabled')->defaultTrue()->end()
-//            ->integerNode('min_sunshine')->defaultValue(3)->end()
             ->end();
+    }
+
+    public function getPaths(): array
+    {
+        $dir = realpath(__DIR__ . '/../assets/');
+        assert(file_exists($dir), 'asset path must exist for the assets in ' . __DIR__);
+        return [$dir => '@survos/reveal'];
     }
 }

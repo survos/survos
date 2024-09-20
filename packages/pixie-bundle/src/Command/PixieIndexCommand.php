4e0d5e5046d44873dde188daa04dbaab21ffcb22
Tac Tacelosky <tacman@gmail.com>
2024-09-20 11:52:16 -0400

    dispatch post-index event

--- packages/pixie-bundle/src/Command/PixieIndexCommand.php
+++ packages/pixie-bundle/src/Command/PixieIndexCommand.php
@@ -7,6 +7,7 @@ use App\Service\TranslationService;
 use Meilisearch\Endpoints\Indexes;
 use Psr\Log\LoggerInterface;
 use Survos\ApiGrid\Service\MeiliService;
+use Survos\PixieBundle\Event\IndexEvent;
 use Survos\PixieBundle\Event\StorageBoxEvent;
 use Survos\PixieBundle\Model\Config;
 use Survos\PixieBundle\Service\PixieService;
@@ -211,6 +212,8 @@ final class PixieIndexCommand extends InvokableServiceCommand
             $stats = $index->stats();
 //            dd($stats, $index->getSettings());
             $io->success($stats['numberOfDocuments'] . " documents");
+            $this->eventDispatcher->dispatch(new IndexEvent($pixieCode, $tableName, $stats['numberOfDocuments']));
+
             $table = new Table($this->io());
             $table->setHeaders(['attributes', 'value']);
 //            $io->write(json_encode($stats, JSON_PRETTY_PRINT));

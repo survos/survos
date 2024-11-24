18c0bde3b87828b2a88c6f57b8986132cd066682
Tac Tacelosky <tacman@gmail.com>
2024-11-24 09:56:56 -0500

    prepare release

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -37,7 +37,7 @@ return static function (MBConfig $mbConfig): void {
     // release workers - in order to execute
     $mbConfig->workers([
         UpdateReplaceReleaseWorker::class,
-        SetCurrentMutualDependenciesReleaseWorker::class,
+//        SetCurrentMutualDependenciesReleaseWorker::class,
         AddTagToChangelogReleaseWorker::class,
         TagVersionReleaseWorker::class,
         PushTagReleaseWorker::class,

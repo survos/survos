40ae56298380bd51757d03f25f021e2fc750e9a6
Tac Tacelosky <tacman@gmail.com>
2024-11-26 13:58:30 -0500

    normalize

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -22,11 +22,23 @@ class AppController extends AbstractController
     #[Route('/', name: 'app_homepage')]
     public function index(PackageService $packageService): Response
     {
-        $packages = $packageService->getPackages();
         $composerReader = $packageService->getProjectComposerJson();
+        $data = $composerReader->getContent();
+        $scripts = $data['scripts'];
+        $scripts['phpstan'] = 'vendor/bin/phpstan';
+        $data['scripts'] = $scripts;
+        $composerReader->writeContent($data);
+        $composerReader->writeContent($data);
+        $composerReader->runCommand('normalize');
+        $composerReader->runCommand('c:c');
+        dd($composerReader->file);
+
+        dd($composerReader, $composerReader->getContent(), get_class_methods($composerReader));
+        $packages = $packageService->getPackages();
         $requires = new RequireDevSection($composerReader);
         $new = new Package($composerReader, 'survos/installer', '^1.5');
         $requires->add($new);
+
 
         return $this->render('app/index.html.twig', [
             'packages' => $packages,

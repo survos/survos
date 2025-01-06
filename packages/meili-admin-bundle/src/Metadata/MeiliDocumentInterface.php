20f4cc66cb4cdbcadef2e603e5127992e0d01b7c
Tac Tacelosky <tacman@gmail.com>
2025-01-06 17:56:37 -0500

    same entities

---
+++ packages/meili-admin-bundle/src/Metadata/MeiliDocumentInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Survos\MeiliAdminBundle\Metadata;
+
+interface MeiliDocumentInterface
+{
+
+}
---
+++ packages/meili-admin-bundle/src/Trait/MeiliDocumentTrait.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Survos\MeiliAdminBundle\Trait;
+
+trait MeiliDocumentTrait
+{
+
+}

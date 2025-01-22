9512eaaef47171eb338a1eff00c9bd3de557a7a8
Tac Tacelosky <tacman@gmail.com>
2025-01-22 09:55:45 -0500

    add DTO, more endpoints

---
+++ packages/libre-translate-bundle/src/Dto/TranslationPayload.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Survos\LibreTranslateBundle\Dto;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class TranslationPayload
+{
+    public const ENGINES = ['libre','bing','piglatin'];
+
+    public function __construct(
+        #[Assert\Locale(canonicalize: true)]
+        public string $from,
+        #[Assert\Choice(choices: self::ENGINES, message: 'Choose a valid engine.')]
+        public string $engine,
+        #[Assert\NotBlank()]
+        public array $to = [],
+        #[Assert\NotBlank()]
+        public array $text = [],
+        public ?string $callbackUrl = null,
+    ) {
+    }
+}

162040fb5f0911cc36e9697c97df9186efafe4ad
Tac Tacelosky <tacman@gmail.com>
2025-05-23 10:22:13 -0400

    fix order

--- packages/code-bundle/src/Service/GeneratorService.php
+++ packages/code-bundle/src/Service/GeneratorService.php
@@ -26,9 +26,9 @@ class GeneratorService
     //    private PropertyAccessor $propertyAccessor;
     public function __construct(
         #[Autowire('%kernel.project_dir%')] private string $projectDir,
-        private ?ManagerRegistry $doctrine=null,
         #[AutowireIterator('workflow.state_machine')] private iterable $workflows,
         #[AutowireIterator('doctrine.repository_service')] private iterable $repositories,
+        private ?ManagerRegistry $doctrine=null,
     )
     {
     }

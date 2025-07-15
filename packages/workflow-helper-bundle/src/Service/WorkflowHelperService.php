7bd67690994d4973bc3e8a1b1aace7e90cd052b4
Tac Tacelosky <tacman@gmail.com>
2025-07-15 08:37:09 -0400

    cleanup prompts and options for iterate

--- packages/workflow-helper-bundle/src/Service/WorkflowHelperService.php
+++ packages/workflow-helper-bundle/src/Service/WorkflowHelperService.php
@@ -290,10 +290,11 @@ class WorkflowHelperService
     public function handleTransition(AsyncTransitionMessage $message)
     {
         $object = $this->entityManager->find($message->getClassName(), $message->getId());
-        assert($object, sprintf( "missing entity %s for %s", $message->getClassName(), $message->getId()));
+        $debugMessage = sprintf( "missing entity %s for %s", $message->getClassName(), $message->getId());
+//        assert($object, $message);
         // removed, throw error (above) during testing only.
         if (!$object) {
-            return;
+            return ['message' => $debugMessage];
         }
         if (!$flowName = $message->getWorkflow()) {
             // ..
@@ -305,19 +306,24 @@ class WorkflowHelperService
             foreach ($workflow->buildTransitionBlockerList($object, $transition) as $blocker) {
                 $this->logger->info($blocker->getMessage());
             }
-            return;
-        }
-        if ($workflow->can($object, $transition)) {
+            return ['message' => $blocker->getMessage()];
+        } else {
             $marking = $workflow->apply($object, $transition, $message->getContext());
             // is this the best place to flush?  or only if workflow applied
             $this->entityManager->flush(); // save the marking and any updates
-        } else {
-            $this->logger?->info("cannot transition from {$object->getMarking()} to $transition");
+
         }
+//        if ($workflow->can($object, $transition)) {
+//            $marking = $workflow->apply($object, $transition, $message->getContext());
+//            // is this the best place to flush?  or only if workflow applied
+//            $this->entityManager->flush(); // save the marking and any updates
+//        } else {
+//            $this->logger?->info("cannot transition from {$object->getMarking()} to $transition");
+//        }
 
         // so we have it for the monitor.
         return [
-            'message' => $message,
+            'message' => json_encode((array)$message),
         ];
 
 
